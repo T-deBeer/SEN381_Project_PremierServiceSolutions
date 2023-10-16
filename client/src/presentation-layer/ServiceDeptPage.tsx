1e84579852ac13125b4ac68febb90fd98271b990
Juanro-2502 <110464132+Juanro-M@users.noreply.github.com>
2023-10-16 14:28:54 +0200

    Started service Department page and setup route

---
+++ client/src/presentation-layer/ServiceDeptPage.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import WelcomeDiv from "../components/WelcomeDiv";
+
+export default function LandingPage() {
+return (
+    <div className="ServiceDeptPage">
+        <Navbar />
+        
+        <Footer /> 
+    </div>
+    );
+}
