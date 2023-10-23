2159e140b47a682fc28fca8dfe2344520db35bf4
Juanro-2502 <110464132+Juanro-M@users.noreply.github.com>
2023-10-23 13:54:13 +0200

    Completed Service Department Page

--- client/src/presentation-layer/ServiceDeptPage.tsx
+++ client/src/presentation-layer/ServiceDeptPage.tsx
@@ -1,13 +1,60 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import WelcomeDiv from "../components/WelcomeDiv";
+import Sidebar, { SidebarProps }  from "../components/Sidebar";
+import { useUser } from "../data-layer/context-classes/UserContext";
 
 export default function LandingPage() {
-return (
-    <div className="ServiceDeptPage">
+    const { user } = useUser();
+    let data: SidebarProps = {
+      showButtons: false,
+      tabContent1: <p>Tab 1 content</p>,
+      tabContent2: <p>Tab 2 content</p>,
+      tabContent3: <p>Tab 3 content</p>,
+    };
+  
+    return (
+      <div className="vh-100">
         <Navbar />
         
-        <Footer /> 
-    </div>
+        <div className="d-flex flex-row gap-3 p-2 h-75">
+          <Sidebar {...data} />
+  
+          <div className="flex-grow-1 h-75">
+            <div className="mb-5">
+              <h2>Unassigned Service Request</h2>
+              <table className="table table-responsive table-dark rounded-3 hover">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Assign Request</th>
+                    <th>Reject Request</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+            <div className="mb-2">
+              <h2>Assigned Service Request</h2>
+              <table className="table table-responsive table-dark rounded-3 hover">
+                <thead>
+                  <tr>
+                  <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Re-Assign Request</th>
+                    <th>Cancel Job</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+  
+        <Footer />
+      </div>
     );
 }
