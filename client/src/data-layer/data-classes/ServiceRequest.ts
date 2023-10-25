931cec2c6b19e8a19c081fc93cc414036e4b11ee
NewToProgramming58 <107695464+NewToProgramming58@users.noreply.github.com>
2023-10-25 08:50:25 +0200

    Updated service request class

--- client/src/data-layer/data-classes/ServiceRequest.ts
+++ client/src/data-layer/data-classes/ServiceRequest.ts
@@ -1,16 +1,22 @@
 import { v4 as uuidv4 } from 'uuid';
 import ServiceClient from './ServiceClient';
-
+import Staff from './Staff';
 
 class ServiceRequest {
   RequestID: string;
   RequestClient: ServiceClient;
+  Staff: Staff;
   Priority: number;
+  ServiceRequestTime: Date;
+  ServiceFulfillmentDate: Date;
 
-  constructor(client: ServiceClient, priority: number) {
+  constructor(client: ServiceClient, priority: number, staff: Staff, serviceRequestTime: Date, serviceFulfillmentTime: Date) {
     this.RequestID = uuidv4();
     this.RequestClient = client;
+    this.Staff = staff;
     this.Priority = priority;
+    this.ServiceRequestTime = serviceRequestTime;
+    this.ServiceFulfillmentDate = serviceFulfillmentTime;
   }
 }
 
